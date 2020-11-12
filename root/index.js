0183bf8194b340f623dfec04784e3ce1faa921da
Nicolasfmelo <73129502+nicolasfmelo@users.noreply.github.com>
2020-11-12 18:07:03 -0300

    Teste

---
+++ root/index.js
@@ -0,0 +1,19 @@
+const add = document.createElement("span")
+add.className = "social"
+const social = document.createElement("a") 
+social.href = 'https://t.me/Nicolas_melo_CV';
+social.innerText = 'Telegram';
+add.appendChild(social)
+const target = {
+    currentFollowers: "https://t.me/Nicolas_melo_CV",
+    btn: document.querySelector("a.btn"),
+    fw: document.querySelector("span.followers")
+  };
+  
+  const follow = () => {
+    target.btn.innerHTML = 'Contato <i class="fas fa-user-times"></i>';
+    target.fw.textContent = target.currentFollowers;
+    target.fw.replaceWith(add)
+    target.btn.classList.toggle("following");
+  }
+  
